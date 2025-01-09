3f95641135dd8592194d7b8e503bd8a56b897c83
SuoweiHu <suowei.h@gmail.com>
2025-01-09 11:03:21 +1100

    Feat: basic post_update code that changes the contactus form reply message

---
+++ mymodule.post_update.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * implment one of the the post_update hooks
+ * Update "Contact Us" from to have areply message
+ */
+function mymodule_post_update_change_contactus_reply(){
+    $contact_form->setReply(t('Thank you for contacting us, we will reply shortly'));
+    $contact_form->save();
+}
+
+
+
