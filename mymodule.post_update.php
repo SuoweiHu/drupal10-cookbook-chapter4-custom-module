6b5abf27614afa72135b27ac8ff063d7e557d81f
SuoweiHu <suowei.h@gmail.com>
2025-01-09 11:39:21 +1100

    Feat: functional schema update hook

--- mymodule.post_update.php
+++ mymodule.post_update.php
@@ -1,10 +1,12 @@
 <?php
 
+
 /**
  * implment one of the the post_update hooks
  * Update "Contact Us" from to have areply message
  */
 function mymodule_post_update_change_contactus_reply(){
+    $contact_form=\Drupal\contact\Entity\ContactForm::load('contactus');
     $contact_form->setReply(t('Thank you for contacting us, we will reply shortly'));
     $contact_form->save();
 }
